bee49d7a9903775f4450cf5d838678001262be92
Maxwell Aruakpor <maxgudly@gmail.com>
2023-12-06 13:33:47 -0500

    building color mode switch

--- purse-pos-app/src/components/NavBar.tsx
+++ purse-pos-app/src/components/NavBar.tsx
@@ -1,11 +1,14 @@
-import { HStack, Image, Text } from '@chakra-ui/react'
-import logo from '../assets/react.svg'
+import { HStack, Image, Spacer, Text } from '@chakra-ui/react'
+import logo from '../assets/flat-logo.svg'
+import ColorModeSwitch from './ColorModeSwitch'
 
 const NavBar = () => {
   return (
-    <HStack>
+    <HStack justifyContent='space-between' padding='10px'>
         <Image src={logo} boxSize='50px'/>
-        <Text>Purse</Text>
+        <Text>FlatPurse App</Text>
+        <Spacer/>
+        <ColorModeSwitch/>
     </HStack>
   )
 }
